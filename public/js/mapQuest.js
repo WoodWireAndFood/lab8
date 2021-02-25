709dd74280ac41bd2268afc7a15b9a078c36ccaf
Mark <gitcommits@mkwsystems.com>
2021-02-25 09:33:12 -0800

    Lab8 finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'gQodwrltFFj1CQea0QRUHVP8wprwWFR1';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878626, -117.235895],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878626, -117.235895]).addTo(map);
 }
